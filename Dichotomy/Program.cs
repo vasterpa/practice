5b457a487bfcd0e07df002800d8edc51a77a2eab
vasterpa <63544122+vasterpa@users.noreply.github.com>
2020-07-09 10:46:37 +0300

    Add files via upload

---
+++ Dichotomy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Dichotomy.Solve(new Fraction(), new Fraction(1, 2), (x) => 8 * x * x * x * x * x - 9 * x + 1, new Fraction(1, 1000));
+
+            try
+            {
+
+                Console.WriteLine(result.GetDecimalRepresentation(4));
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
