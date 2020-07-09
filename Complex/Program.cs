72355046a489b8871e7082a5d003804a2c7fa971
vasterpa <63544122+vasterpa@users.noreply.github.com>
2020-07-09 10:47:14 +0300

    Add files via upload

---
+++ Complex/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fr1 = new Fraction(2, 5);
+            var fr2 = new Fraction(2, 5);
+            var complex = new Complex(fr1, fr2);
+            
+            try
+            {
+
+                Console.WriteLine((complex).ToString());
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
